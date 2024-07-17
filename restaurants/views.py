83298d892481a02c4ad038f1d3971b0f0c06d5df
Naveen Madugula <naveenmadugula7630@gmail.com>
2024-07-17 10:33:00 +0530

    added api webservices

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from .models import Restaurant
+from .serializers import RestaurantSerializer
+from rest_framework.pagination import PageNumberPagination
+
 
 # Create your views here.
+class RestaurantPagination(PageNumberPagination):
+  page_size = 10
+  page_size_query_param = 'page_size'
+  max_page_size = 100
+  
+
+class RestaurantDetail(generics.RetrieveAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    
+class RestaurantList(generics.ListCreateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    pagination_class = RestaurantPagination
