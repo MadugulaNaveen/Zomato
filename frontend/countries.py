dd609cf4950797f97328d0558e438500519bdf98
Naveen Madugula <naveenmadugula7630@gmail.com>
2024-07-27 15:55:36 +0530

    changed path

--- frontend/countries.py
+++ frontend/countries.py
@@ -1,3 +1,8 @@
+import os
 import pandas as pd
-df = pd.read_excel('/Users/naveenm/Coding/placementDrives/task-MadugulaNaveen/Country-Code.xlsx', header=None)
+from django.templatetags.static import static
+from django.conf import settings
+
+Excelpath =os.path.join(settings.BASE_DIR,'static','Data','Country-Code.xlsx')
+df = pd.read_excel(Excelpath, header=None)
 data_dict = dict(zip(df.iloc[1:, 1],df.iloc[1:, 0]))
